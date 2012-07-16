8a2611ff6c3ca7a557001b7a95960c112ce24fce
Peter Downs <peter.l.downs@gmail.com>
2012-07-15 18:11:47 -0700

    Uses new template wrapper for homepage

--- python/src/index.py
+++ python/src/index.py
@@ -1,15 +1,14 @@
 # coding: utf-8
 from flask import (redirect, make_response)
+from templates import (Render)
 from src import (app)
+
+class Index(object):
+	pass
 
 @app.route('/', methods=['GET'])
 def index():
-	return """
-	<center>
-	<h1>Hello, world!</h1>
-	<img src='/static/celebration_dance_addams_family.gif' />
-	</center>
-	"""
+	return Render('index', name='World')
 
 @app.route('/<path:path>/', methods=['GET', 'PUT', 'POST', 'DELETE', 'HEAD'])
 def error_handler(path):
