3363f05f531ca91b547aa77f9ff6803c67013d65
Peter Downs <peter.l.downs@gmail.com>
2012-07-15 17:12:01 -0700

    Adds a funny gif to the homepage

--- python/src/index.py
+++ python/src/index.py
@@ -1,6 +1,16 @@
 # coding: utf-8
-from src import app
+from flask import (redirect, make_response)
+from src import (app)
 
 @app.route('/', methods=['GET'])
 def index():
-	return "Hello, world!"
+	return """
+	<center>
+	<h1>Hello, world!</h1>
+	<img src='/static/celebration_dance_addams_family.gif' />
+	</center>
+	"""
+
+@app.route('/<path:path>/', methods=['GET', 'PUT', 'POST', 'DELETE', 'HEAD'])
+def error_handler(path):
+	return make_response("Page not found", 404)
