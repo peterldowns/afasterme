1f051ec2c9823288e71d337b2629b45c3fe99ebf
Peter Downs <peter.l.downs@gmail.com>
2012-07-22 22:12:12 -0700

    Switches to new pystache

--- python/src/index.py
+++ python/src/index.py
@@ -1,9 +1,10 @@
 # coding: utf-8
 from flask import (redirect, make_response)
-from pystache import (Render, RenderFile as Template)
+from pystache import (template)
 from src import (app)
 
 @app.route('/', methods=['GET'])
+@app.route('static/templates/index.html')
 def index():
 	return Template('index', {'name':'World'})
 
