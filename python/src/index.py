ea2a46eb7e028d832dadd68fc510150a099eb015
Peter Downs <peter.l.downs@gmail.com>
2012-07-23 11:10:57 -0700

    Moves error handler to __init__

--- python/src/index.py
+++ python/src/index.py
@@ -6,8 +6,4 @@ from src import (app)
 @app.route('/', methods=['GET'])
 @template('static/templates/index.html')
 def index():
-	return ({'name':'Peter'},{})
-
-@app.route('/<path:path>/', methods=['GET', 'PUT', 'POST', 'DELETE', 'HEAD'])
-def error_handler(path):
-	return make_response("Page not found", 404)
+    return {'name':'Peter'}, {}
