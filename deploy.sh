54c7a66f80f0d330e73fd7ba78e4593d6f90758a
Peter Downs <peter.l.downs@gmail.com>
2012-07-25 21:50:25 -0700

    Fixes environment sniffing

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,3 @@
 dotcloud push afasterme;
-dotcloud run afasterme.www "cat > current/src/conf.py" < python/src/conf.py
+dotcloud run afasterme.www "cat > current/src/conf.py" < python/src/conf.py;
+dotcloud run afasterme.www "tail -f /var/logs/supervisor/uwsgi.log";
--- python/src/db.py
+++ python/src/db.py
@@ -1,67 +1,70 @@
 # coding: utf-8
 import json # for loading the env file
-from os import environ # for loading environment varilables
 from pymongo import Connection # for accessing the database
 
 def db_creds(env_path='/home/dotcloud/environment.json'):
-	""" Get DB connection information. """
-	creds = {}
-	if environ.get('ON_DOTCLOUD', False):
-		envf = open(env_path, 'rb')
-		env = json.load(f)
-		envf.close()
-		creds['host'] = env['DOTCLOUD_DB_MONGODB_HOST']
-		creds['port'] = env['DOTCLOUD_DB_MONGODB_PORT']
-		creds['user'] = env['DOTCLOUD_DB_MONGODB_LOGIN']
-		creds['pwd'] = env['DOTCLOUD_DB_MONGODB_PASSWORD']
-	else:
-		creds['host'] = 'localhost'
-		creds['port'] = 27017
-		creds['user'] = 'peterldowns'
-		creds['pwd'] = 'localpass'
-	
-	creds['port'] = int(creds['port'])
-	return creds
+    """ Get DB connection information. """
+    creds, env = {}, {}
+    
+    try:
+        with open(env_path, 'rb') as f:
+            env = json.load(f)
+    except IOError:
+        pass
+    
+    if env.get('DOTCLOUD', False):
+        creds['host'] = env['DOTCLOUD_DB_MONGODB_HOST']
+        creds['port'] = env['DOTCLOUD_DB_MONGODB_PORT']
+        creds['user'] = env['DOTCLOUD_DB_MONGODB_LOGIN']
+        creds['pwd'] = env['DOTCLOUD_DB_MONGODB_PASSWORD']
+    else:
+        creds['host'] = 'localhost'
+        creds['port'] = 27017
+        creds['user'] = 'peterldowns'
+        creds['pwd'] = 'localpass'
+    
+    creds['port'] = int(creds['port'])
+    return creds
 
 class DBConn(object):
-	def __init__(self, host, port, user, pwd):
-		self._conn = None
-		self.host = host
-		self.port = int(port)
-		self.user = user
-		self.pwd = pwd
-		self.authenticate()
-	
-	def authenticate(self):
-		self._conn = Connection(self.host, self.port)
-		self._db = self._conn['admin']
-		if not self._db.authenticate(self.user, self.pwd):
-			raise Exception("Unable to authenticate")
-		return self
-	
-	def db(self, db_name):
-		self._db = self._conn[db_name]
-		return self
-	
-	def collection(self, coll_name):
-		self._coll = self._db[coll_name]
-		return self
+    def __init__(self, host, port, user, pwd):
+        self._conn = None
+        self.host = host
+        self.port = int(port)
+        self.user = user
+        self.pwd = pwd
+        self.authenticate()
+    
+    def authenticate(self):
+        self._conn = Connection(self.host, self.port)
+        self._db = self._conn['admin']
+        if not self._db.authenticate(self.user, self.pwd):
+            raise Exception("Unable to authenticate")
+        return self
+    
+    def db(self, db_name):
+        self._db = self._conn[db_name]
+        return self
+    
+    def collection(self, coll_name):
+        self._coll = self._db[coll_name]
+        return self
 
-	def find(self, params={}, sort_params={}):
-		if params:
-			resp = self._coll.find(params)
-		else:
-			resp = self._coll.find()
+    def find(self, params={}, sort_params={}):
+        if params:
+            resp = self._coll.find(params)
+        else:
+            resp = self._coll.find()
 
-		if sort_params:
-			return resp.sort(sort_params)
-		else:
-			return resp
+        if sort_params:
+            return resp.sort(sort_params)
+        else:
+            return resp
 
-	def find_one(self, params={}):
-		if params:
-			return self._coll.find_one(params)
-		else:
-			return self._coll.find_one()
+    def find_one(self, params={}):
+        if params:
+            return self._coll.find_one(params)
+        else:
+            return self._coll.find_one()
 
 dbc = DBConn(**db_creds())
