b9ede5e7953f385a51594c4a14c472046f3337cf
Peter Downs <peter.l.downs@gmail.com>
2012-07-25 21:59:48 -0700

    Fixes typo (logs -> log)

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 dotcloud push afasterme;
 dotcloud run afasterme.www "cat > current/src/conf.py" < python/src/conf.py;
-dotcloud run afasterme.www "tail -f /var/logs/supervisor/uwsgi.log";
+dotcloud run afasterme.www "tail -f /var/log/supervisor/uwsgi.log";
