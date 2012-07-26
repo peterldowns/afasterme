46f56be79979baff3c7f739fa4468204d41de02e
Peter Downs <peter.l.downs@gmail.com>
2012-07-25 18:42:22 -0700

    Tries to add FB login

--- python/src/auth.py
+++ python/src/auth.py
@@ -1,11 +1,82 @@
 # coding: utf-8
-from src import app # circular as fuck
+import requests
 from bottle import (redirect, abort, request)
 from pystache import (template)
+
+from src import app # circular as fuck
 from db import (dbc)
 from util import (session)
+from conf import (fb_perms, fb_app_id)
 
 ROOT_URL = '/'
+
+@app.get('/fblogin')
+@app.get('/fblogin/')
+@template('static/templates/fblogin.html')
+def fblogin():
+    return {}, {}
+
+@app.post('/fblogin')
+@app.post('/fblogin/')
+def fblogin_handler():
+    s = session()
+    fburl = (
+        'https://www.facebook.com/dialog/oauth?client_id={}'.format(fb_app_id)+
+        '&redirect_uri={}'.format('http://localhost:8080/fbredirect')+
+        '&scope={}'.format(','.join(fb_perms))+
+        '&state={}'.format(s['_csrf'])
+    )
+    print "Redirect URL =", fburl
+    redirect(fburl)
+
+@app.get('/fbredirect')
+@app.get('/fbredirect/')
+def fblogin_redirect():
+    url = (
+        'https://graph.facebook.com/oauth/access_token?client_id={}'.format(fb_app_id)+
+        '&redirect_uri={}'.format('http://localhost:8080/fbredirect')+
+        '&client_secret={}'.format(fb_app_secret)+
+        '&code={}'.format(request.query.code)
+    )
+    resp = requests.post(url)
+    print resp.status_code
+    if resp.status_code == 200:
+        print resp.content
+    else:
+        redirect('/')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 @app.get('/login')
 @app.get('/login/')
