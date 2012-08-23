4a98f654dc698f27f2fc53200817a0a410eb5e54
Peter Downs <peterldowns@gmail.com>
2012-08-23 00:24:59 -0700

    Updates DB code to include latest changes to DBConn (now DBC)

--- python/src/db.py
+++ python/src/db.py
@@ -26,45 +26,66 @@ def db_creds(env_path='/home/dotcloud/environment.json'):
     creds['port'] = int(creds['port'])
     return creds
 
-class DBConn(object):
-    def __init__(self, host, port, user, pwd):
+class DBC(object):
+    def __init__(self, host, port, user=None, pwd=None):
         self._conn = None
-        self.host = host
+        self._db = None
+        self._coll = None
+
+        self.host = str(host)
         self.port = int(port)
-        self.user = user
-        self.pwd = pwd
-        self.authenticate()
+        self.user = str(user)
+        self.pwd = str(pwd)
     
-    def authenticate(self):
-        self._conn = Connection(self.host, self.port)
-        self._db = self._conn['admin']
-        if not self._db.authenticate(self.user, self.pwd):
-            raise Exception("Unable to authenticate")
-        return self
-    
+    def __repr__(self):
+        if self._db:
+            return repr(self._db)
+        return '<DBC {}:{} (not connected)>'.format(self.host, self.port)
+
+    def __str__(self):
+        if self._db:
+            return str(self._db)
+        return '<DBC {}:{} (not connected)>'.format(self.host, self.port)
+
+    def __unicode__(self):
+        if self._db:
+            return unicode(self._db)
+        return u'<DBC {}:{} (not connected)>'.format(self.host, self.port)
+
+    def __call__(self, *args):
+        return self.db(*args)
+
+    def use(self, *args):
+        return self.db(*args)
+
     def db(self, db_name):
+        if not self._conn:
+            self._conn = Connection(self.host, self.port)
         self._db = self._conn[db_name]
         return self
     
-    def collection(self, coll_name):
-        self._coll = self._db[coll_name]
+    def auth(self, user=None, pwd=None):
+        self.user = str(user) or self.user
+        self.pwd = str(pwd) or self.pwd
+        if not self._db:
+            raise Exception("Unable to Authenticate: not connected to a database.")
+        if not self._db.authenticate(self.user, self.pwd):
+            raise Exception("Unable to Authenticate: invalid credentials.")
+        return self
+   
+    def coll(self, *args):
+        return self.collection(*args)
+
+    def collection(self, c_name):
+        if not self._db:
+            raise Exception("Unable to use collection {}: not connected to a database.".format(repr(c_name)))
+        self._coll = self._db[c_name]
         return self
 
-    def find(self, params={}, sort_params={}):
-        if params:
-            resp = self._coll.find(params)
-        else:
-            resp = self._coll.find()
+    def __getattr__(self, key):
+        try:
+            return getattr(self._coll, key)
+        except AttributeError:
+            raise AttributeError('DB Connection has no attribute {}'.format(repr(key)))
 
-        if sort_params:
-            return resp.sort(sort_params)
-        else:
-            return resp
-
-    def find_one(self, params={}):
-        if params:
-            return self._coll.find_one(params)
-        else:
-            return self._coll.find_one()
-
-dbc = DBConn(**db_creds())
+dbc = DBC(**db_creds())
